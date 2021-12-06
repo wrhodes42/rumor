3bd5602dca03fdea8d9d263fb8d1f148cf6aae4b
Will Rhodes <wrhodes42@gmail.com>
2021-12-06 11:04:55 -0500

    removed archive from app and studio

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Archive from '../views/Archive.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/archive',
-    name: 'Archive',
-    component: Archive
   }
 ]
 
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -6,7 +6,6 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
 import blockContent from './blockContent'
-import post from './post'
 import config from './config'
 import link from './iFrameLink'
 
@@ -19,7 +18,6 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    post,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
