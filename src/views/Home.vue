b2b42c672e5b2875e08133fefda75ba573cb7b5b
Will Rhodes <wrhodes42@gmail.com>
2022-11-10 09:01:01 -0500

    mobile signup form
    Updated the mobile version of the signup form to use Mailchimp, was using an out-of-use formspree from the last iteration of the site.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -191,12 +191,50 @@
                   </div>
                   
                   <div id="receive-simple" ref='receiveSimple'>
-                      <form id="receive-form-simple" method="post" action="https://formspree.io/rumor.pu@gmail.com">
-                          <input class="h1" type="text" placeholder="NAME" name="name">
-                          <input class="h1" type="email" name="email" placeholder="EMAIL">
 
-                          <input type="submit" value="SIGN UP" class="h1 form-submit">
-                      </form>
+                      <!-- Begin Mailchimp Signup Form -->
+                        <div id="mc_embed_signup" class="receive-form-simple">
+                        <form action="https://gmail.us20.list-manage.com/subscribe/post?u=4247af630a4934b76e5791b30&amp;id=970821ba7f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate receive-form-b" target="_blank" novalidate>
+                            <div id="mc_embed_signup_scroll">
+                            <!--<h1>Subscribe</h1>-->
+                        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                        <div class="mc-field-group">
+                            <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                        </label>
+                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                        </div>
+                        <div class="mc-field-group">
+                            <label for="mce-FNAME">First Name </label>
+                            <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                        </div>
+                        <div class="mc-field-group">
+                            <label for="mce-LNAME">Last Name </label>
+                            <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                        </div>
+                        <div class="mc-field-group size1of2">
+                            <label for="mce-BIRTHDAY-month">Birthday </label>
+                            <div class="datefield">
+                                <span class="subfield monthfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="MM" size="2" maxlength="2" name="BIRTHDAY[month]" id="mce-BIRTHDAY-month"></span> / 
+                                <span class="subfield dayfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="DD" size="2" maxlength="2" name="BIRTHDAY[day]" id="mce-BIRTHDAY-day"></span> 
+                                <span class="small-meta nowrap">( mm / dd )</span>
+                            </div>
+                        </div>	<div id="mce-responses" class="clear foot">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4247af630a4934b76e5791b30_970821ba7f" tabindex="-1" value=""></div>
+                                <div class="optionalParent">
+                                    <div class="clear foot">
+                                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                                        <!--
+                                            <p class="brandingLogo"><a href="http://eepurl.com/hOiox1" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"></a></p>
+                                        -->
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        </div>
+                        <!--End Mailchimp Signup Form-->
                   </div>
 
                   <div id="submit-simple" ref='submitSimple'>
